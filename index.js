f4262c3d2258c214e2e07c637e9ac15a21d6e391
domilgarg <domil.garg@paytm.com>
2020-05-31 12:29:15 +0530

    Adding Tiktok trending videos.

---
+++ index.js
@@ -0,0 +1,20 @@
+const TikTokScraper = require('tiktok-scraper');
+
+// usefullink 
+// scrate trending videos of tiktok
+(async () => {
+    try {
+        const posts = await TikTokScraper.trend('', { number: 100, filetype:'csv'});
+        // console.log(posts);
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+// scrape search results of you tube
+// // useful link   : https://github.com/DrKain/scrape-youtube
+// const youtube = require('scrape-youtube').default;
+
+// youtube.searchOne('Short Change Hero').then(video => {
+//     console.log(JSON.stringify(video, null, 2));
+// }).catch(console.error);;
